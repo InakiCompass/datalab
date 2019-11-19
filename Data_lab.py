de6506605036bc643e66ebb69a6292187b215cd4
inakicompass <inaki.errazuriz@cgcompas.com>
2019-11-19 18:19:44 -0300

    lk

--- Data_lab.py
+++ Data_lab.py
@@ -3,7 +3,6 @@ import dash_auth
 import dash_table
 import pandas as pd
 import flask
-import plotly
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
